2dd4577e94eb28025ea2d1ea925626e4ee3b181a
Bjørn Madsen <bm@aeons.dk>
2017-08-14 20:59:23 +0200

    wip

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,9 +1,33 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: "./js/app.js",
   output: {
     path: path.resolve(__dirname, "../priv/static/js"),
     filename: "app.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: "elm-webpack-loader",
+          options: {}
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
   }
-}
+};
--- lib/todo_web/router.ex
+++ lib/todo_web/router.ex
@@ -14,13 +14,8 @@ defmodule TodoWeb.Router do
   end
 
   scope "/", TodoWeb do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", TodoWeb do
-  #   pipe_through :api
-  # end
 end
